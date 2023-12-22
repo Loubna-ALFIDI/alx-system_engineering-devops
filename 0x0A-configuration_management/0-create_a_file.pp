7b6a1c40fab3ea8f61cba79def1c70eb625b453e
Loubna <loubna.alfidi@gmail.com>
2023-12-22 23:47:19 +0100

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create file school in /tmp
+
+file { '/tmp/school':
+    ensure  => present,
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+}
