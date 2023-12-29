7988344203c5f2a1df490fa50e6dc9bc0bc2e51d
Loubna <loubna.alfidi@gmail.com>
2023-12-29 22:15:56 +0100

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,13 @@
 # Client configuration file (w/ Puppet)
 
 file_line { 'Turn off passwd auth':
-    ensure => present,
-    path => '/etc/ssh/ssh_config;,
-    line => 'PasswordAuthentification no',
+  ensure => present,
+  line   => '    PasswordAuthentication no',
+  path   => '/etc/ssh/ssh_config',
 }
 
 file_line { 'Declare identity file':
-    path => '/etc/ssh/ssh_config',
-    line => 'IdentityFile ~/.ssh/school',
+  ensure => present,
+  line   => '    IdentityFile ~/.ssh/school',
+  path   => '/etc/ssh/ssh_config',
 }
