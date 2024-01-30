11024ee4ff7e64941c0c46db7f26523e46ba384d
Loubna <loubna.alfidi@gmail.com>
2024-01-30 01:41:21 +0100

    Export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -10,7 +10,7 @@ import sys
 if __name__ == "__main__":
     """ Gather data from an API"""
     id_user = sys.argv[1]
-    url = f'https://jsonplaceholder.typicode.com/'
+    url = 'https://jsonplaceholder.typicode.com/'
     userid = f'users?id={id_user}'
     user_response = requests.get(f'{url}{userid}')
     user = user_response.json()
@@ -18,10 +18,6 @@ if __name__ == "__main__":
     done = f'{todos}&completed=true'
     todo_response = requests.get(f'{url}{todos}')
     todo = todo_response.json()
-    Name = user[0].get("name")
-    todosdone = requests.get(f'{url}{done}').json()
-    todos_completed = len(todosdone)
-    totaldone = len(todo)
     userName = user[0].get("username")
 
     with open(f'{id_user}.csv', 'w') as f:
