91c09837c89fb5d1cd6b9c73e9a370e75dfc1064
Loubna <loubna.alfidi@gmail.com>
2024-02-14 02:28:41 +0100

    Top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+1-top_ten
+"""
+import requests
+
+
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0\
+        Safari/537.36'}
+
+
+def top_ten(subreddit):
+    """method doc"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        for post in data["data"]["children"]:
+            print(post["data"]["title"])
+    else:
+        print("None")
