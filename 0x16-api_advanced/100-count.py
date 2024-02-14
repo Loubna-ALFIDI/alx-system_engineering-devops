e75ae4dd745fea57e7080ba961df2e0feea2a725
Loubna <loubna.alfidi@gmail.com>
2024-02-14 02:31:42 +0100

    Recurse it

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""
+100-count
+"""
+
+import requests
+
+
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0\
+        Safari/537.36'}
+
+
+def count_words(subreddit, word_list):
+    """Method doc"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=100".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        count = {}
+        for post in data["data"]["children"]:
+            for word in word_list:
+                # print(word)
+                if word in post["data"]["title"].lower():
+                    if word in count:
+                        count[word] += 1
+                    else:
+                        count[word] = 1
+        return count
+    else:
+        return None
